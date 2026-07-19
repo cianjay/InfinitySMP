c0f9e69a272658188ec576e34acc4299ddf65b62
cianjay <137462741+cianjay@users.noreply.github.com>
2026-07-19 18:49:51 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -121,7 +121,7 @@ export const botConfig = {
         ended: "#ED4245",
       },
       ticket: {
-        open: "#57F287",
+        open: "#32CD32",
         claimed: "#FAA61A",
         closed: "#ED4245",
         pending: "#99AAB5",
@@ -223,7 +223,7 @@ export const botConfig = {
     priorities: {
       none: {
         emoji: "⚪",
-        color: "",
+        color: "#57F287",
         label: "None",
       },
       low: {
