268c0856b07aeda25b61d9d2c53d867fbeb02fda
cianjay <137462741+cianjay@users.noreply.github.com>
2026-07-19 18:20:15 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -94,7 +94,7 @@ export const botConfig = {
     colors: {
       // Main brand colors.
       primary: "#008000",
-      secondary: "#2F3136",
+      secondary: "#008000",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
